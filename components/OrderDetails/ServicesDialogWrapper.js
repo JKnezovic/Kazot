2526559ad1deb8e69e2092ac45ffc4498ac37560
JKnezovic <jo.knezovic@gamil.com>
2024-10-08 18:21:02 +0200

    fixed bug where keyboard was shown over dialog on ipad

--- components/OrderDetails/ServicesDialogWrapper.js
+++ components/OrderDetails/ServicesDialogWrapper.js
@@ -1,6 +1,7 @@
+import { useState, useEffect } from "react";
 import { Dialog, Portal } from "react-native-paper";
 import { moderateScale } from "../../Scaling";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Platform, Keyboard } from "react-native";
 import ServicesDoneDialog from "./ServicesDoneDialog";
 import DeleteDialog from "../Inventory/Dialogs/DeleteDialog";
 
@@ -14,10 +15,35 @@ const ServicesDialogWrapper = ({
   setIsDelete,
   isDelete,
 }) => {
+  const [bottom, setBottom] = useState(0);
+  useEffect(() => {
+    function onKeyboardChange(e) {
+      // Check if the event has the required properties, code breaks on android without this
+      const hasCoordinates = e.endCoordinates && e.startCoordinates;
+      if (Platform.OS === "ios" && hasCoordinates) {
+        if (e.endCoordinates.screenY <= e.startCoordinates.screenY) {
+          setBottom(e.endCoordinates.height / 2);
+        } else {
+          setBottom(0);
+        }
+      }
+    }
+
+    if (Platform.OS === "ios") {
+      const subscription = Keyboard.addListener("keyboardWillChangeFrame", onKeyboardChange);
+      return () => subscription.remove();
+    }
+
+    const subscriptions = [
+      Keyboard.addListener("keyboardDidHide", onKeyboardChange),
+      Keyboard.addListener("keyboardDidShow", onKeyboardChange),
+    ];
+    return () => subscriptions.forEach((subscription) => subscription.remove());
+  }, []);
   return (
     <Portal>
       <Dialog
-        style={styles.dialog}
+        style={[styles.dialog, { bottom }]}
         visible={visible}
         onDismiss={() => setVisible(false)}
       >
